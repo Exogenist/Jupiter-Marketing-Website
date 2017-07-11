4f803d070fddbc5cc36357789dc5c9f6fa176b84
exogenist <swainson.holness@gmail.com>
2017-07-11 12:15:09 -0600

    installed parallax

--- main.js
+++ main.js
@@ -2,6 +2,56 @@ $(document).ready(function () {
     //Variables
     var scroll = 0;
 
+    //parallax plugin. Not my code ------------------------------------------------
+
+    /**
+     * Author: Heather Corey
+     * jQuery Simple Parallax Plugin
+     *
+     */
+    (function ($) {
+
+        $.fn.parallax = function (options) {
+
+            var windowHeight = $(window).height();
+
+            // Establish default settings
+            var settings = $.extend({
+                speed: 0.15
+            }, options);
+
+            // Iterate over each object in collection
+            return this.each(function () {
+
+                // Save a reference to the element
+                var $this = $(this);
+
+                // Set up Scroll Handler
+                $(document).scroll(function () {
+
+                    var scrollTop = $(window).scrollTop();
+                    var offset = $this.offset().top;
+                    var height = $this.outerHeight();
+
+                    // Check if above or below viewport
+                    if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+                        return;
+                    }
+
+                    var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+
+                    // Apply the Y Background Position to Set the Parallax Effect
+                    $this.css('background-position', 'center ' + yBgPosition + 'px');
+
+                });
+            });
+        }
+    }(jQuery));
+    $('.header').parallax({
+        speed: 0.15
+    });
+    //-------------------------------------------------- THANK YOU HEATHER!! END
+
     $(this).scrollTop(0);
     scroll = $(window).scrollTop();
     if (scroll !== 0) {
